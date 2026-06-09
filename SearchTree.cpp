2314d5caf4dfd83bb251bdd1a47ea0e729909cbc
variannn340 <varianvarian0301@gmail.com>
2026-06-09 15:11:40 +0700

    add half of case

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -156,8 +156,22 @@ int main()
         cout << endl;
 
         switch (ch)
-        
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
 
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+        
     
 }
 
