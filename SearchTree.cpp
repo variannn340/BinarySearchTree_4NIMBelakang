db6acabf7cf3a7ea82ebd9471caa7467f63fb582
variannn340 <varianvarian0301@gmail.com>
2026-06-09 14:12:40 +0700

    Constructor for the node class

---
+++ SearchTree.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+        string info;
+        Node *leftchild;
+        Node *rightchild;
+
+        //Constructor for the node class
+        Node(string i, Node *l, Node *r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+};
