e688063739a21e7f37dde593b6a548708f24c0c1
variannn340 <varianvarian0301@gmail.com>
2026-06-09 14:56:18 +0700

    add char ch

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -120,7 +120,26 @@ int main()
 {
     BinaryTree obj;
 
-    
+    while(true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement Insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
+
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+
+        
+
+    }
+
+
 
 
 }
